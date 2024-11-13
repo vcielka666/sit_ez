305afd59c3d9928529a7de4cac835a69994f72a8
vcielka666 <romanmarko90@gmail.com>
2024-11-13 22:05:12 +0100

    Build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,15 @@
-// src/lib/prisma.ts
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  // Prevent multiple instances of Prisma Client in development
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma = global.prisma || new PrismaClient({
+  log: ['query', 'info', 'warn', 'error'], // Optional logging for debugging
+});
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+
 export default prisma;
