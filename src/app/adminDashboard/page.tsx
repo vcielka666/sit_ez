afbc27edf370a367be5a3d6598e5358084fed1a6
vcielka666 <romanmarko90@gmail.com>
2024-11-14 01:42:09 +0100

    testing

--- src/app/adminDashboard/page.tsx
+++ src/app/adminDashboard/page.tsx
@@ -1,16 +1,16 @@
 import DashboardWrapper from "@/components/DashboardWrapper"
 import React from "react";
-// import { auth } from "../../../auth"
-// import { redirect } from "next/navigation";
+import { auth } from "../../../auth"
+import { redirect } from "next/navigation";
 
 
 
 
 const Page = async () =>  {
-    // const session = await auth();
-    // if(!session?.user) {
-    //     redirect("/")
-    // }
+    const session = await auth();
+    if(!session?.user) {
+        redirect("/")
+    }
 
 
     return (
