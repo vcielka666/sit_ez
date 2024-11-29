566cf9c3b34a58f311aee558a15760d38002d15d
vcielka666 <romanmarko90@gmail.com>
2024-11-30 00:46:12 +0100

    UI added test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import Map from "@/components/map/Map"; // Assuming `Map` component is inside `components/Map`
 
-
 export default function UserPage() {
-  const [selectedPlace, setSelectedPlace] = useState(null); // Holds the selected place data for the marker
-
   return (
     <div className="p-4">
       <h1 className="text-xl font-semibold">Discover Pubs and Restaurants</h1>
       <div id="map" className="mt-4 h-[500px]">
-        <Map onMarkerClick={setSelectedPlace} /> {/* Pass callback to handle marker clicks */}
+        <Map onMarkerClick={() => {}} /> {/* No-op callback */}
       </div>
-
     </div>
   );
 }
